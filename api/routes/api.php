cf901e749b1ef813d1aedd7c2baf7eb8ca3306cf
journii gorle <ginopaflo001608@gmail.com>
2026-01-26 12:40:17 -0500

    fix: basic routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,2 +1,40 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+use Modules\Auth\Controllers\AuthController;
+use Modules\User\Controllers\UserController;
+use Modules\Rbac\Controllers\RbacController;
+use Modules\Ubigeo\Controllers\UbigeoController;
+use Modules\Office\Controllers\OfficeController;
+
+Route::prefix('/auth/')->group(function(){
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('refresh', [AuthController::class, 'refresh'])->middleware('jwt:refresh');
+    Route::patch('password', [AuthController::class, 'changePassword'])->middleware('jwt:internal');
+});
+
+Route::prefix('/office/')->group(function(){
+    Route::post('create', [OfficeController::class, 'creater'])->middleware('jwt:internal');
+    Route::get('list', [OfficeController::class, 'lister'])->middleware('jwt:internal');
+    Route::patch('update', [OfficeController::class, 'updater'])->middleware('jwt:internal');
+    Route::delete('remove', [OfficeController::class, 'deleter'])->middleware('jwt:internal');
+});
+
+Route::prefix('rbac')->group(function(){
+    Route::get('role/list', [RbacController::class, 'listerRole'])->middleware('jwt:internal');
+    Route::post('role/create', [RbacController::class, 'createRole'])->middleware('jwt:internal');
+    Route::post('role/assign', [RbacController::class, 'syncPermissionRole'])->middleware('jwt:internal');
+    Route::get('permission/list', [RbacController::class, 'listerPermission'])->middleware('jwt:internal');
+});
+
+Route::prefix('/user/')->group(function(){
+    Route::post('register', [UserController::class, 'register'])->middleware('jwt:internal');
+    Route::get('list', [UserController::class, 'Lister'])->middleware('jwt:internal');
+    Route::patch('update', [UserController::class, 'updater'])->middleware('jwt:internal');
+});
+
+Route::prefix('ubigeo')->group(function(){
+    Route::get('departments', [UbigeoController::class , 'departments']);
+    Route::get('province', [UbigeoController::class, 'provinces']);
+    Route::get('districts',  [UbigeoController::class,  'districts']);
+});
