ece9692989ec8a8331ba9033896b63b3aeeac842
journii gorle <ginopaflo001608@gmail.com>
2026-02-09 15:17:19 -0500

    fixing certifications

--- api/src/Modules/ProfessionalRecords/Applications/ListCertificationsCase.php
+++ api/src/Modules/ProfessionalRecords/Applications/ListCertificationsCase.php
@@ -3,25 +3,26 @@
 namespace Modules\ProfessionalRecords\Applications;
 
 use Modules\ProfessionalRecords\Repositories\CertificationRepository;
+use Modules\Shared\Services\FileUrlHelper;
 
 class ListCertificationsCase
 {
     public function __construct(private CertificationRepository $repository) {}
 
-    public function exec(int $userId): array
+    public function exec(int $userId)
     {
         $records = $this->repository->getByUserId($userId);
+        
+        $file = FileUrlHelper::getFileUrl('professional-records.files.download', 'filePath', $records->file);
 
-        return $records->map(function ($record) {
+        return $records->map(function ($record) use ($file) {
             return [
                 'id' => $record->id,
                 'name' => $record->name,
                 'year' => $record->year,
                 'hours' => $record->hours,
-                'file' => $record->file,
-                'created_at' => $record->created_at->toISOString(),
-                'updated_at' => $record->updated_at->toISOString(),
+                'file' => $file
             ];
-        })->toArray();
+        });
     }
 }
--- api/src/Modules/ProfessionalRecords/Requests/ListCertificationsRequest.php
+++ api/src/Modules/ProfessionalRecords/Requests/ListCertificationsRequest.php
@@ -3,16 +3,13 @@
 namespace Modules\ProfessionalRecords\Requests;
 
 use Modules\Auth\Shared\Requests\Template;
+use Override;
 
 class ListCertificationsRequest extends Template
 {
+    #[Override]
     public function authorize(): bool
     {
-        return $this->verifyPermission(['p.postulante', 'cv.evaluation']);
-    }
-
-    public function rules(): array
-    {
-        return [];
+        return $this->verifyPermission('p.postulante');
     }
 }
